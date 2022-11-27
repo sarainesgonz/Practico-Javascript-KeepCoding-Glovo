e4abea4cbdc4cca8d371892729e358027a0b9b15
Sara Ines Gonzalez <107520803+sarainesgonz@users.noreply.github.com>
2022-11-27 14:22:57 -0300

    ELiminé objetos del array y volví al listado inicial provisto en el challengeg. Funciona con array vacio tambien

--- utils.js
+++ utils.js
@@ -11,18 +11,7 @@ const students = [{
   gender: 'female',
   name: 'Silvia'
 },
-{
-  age: 29,
-  examScores: [],
-  gender: 'female',
-  name: 'Maria'
-},
-{
-  age: 24,
-  examScores: [],
-  gender: 'male',
-  name: 'Diego'
-}]
+]
 
 const availableMaleNames = ['Pepe', 'Juan', 'Victor', 'Leo', 'Francisco', 'Carlos'];
 const availableFemaleNames = ['Cecilia', 'Ana', 'Luisa', 'Susana', 'Isabel', 'Virginia'];
